76b1a5a4b35cf2ad414746e05e4729411f0c8330
Vodyanik <vodyanik.artur@gmail.com>
2025-04-25 18:30:02 +0300

    Example comment

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
 require_once 'vendor/autoload.php';
+
+// git status                            это текущее состояние, делаем всегда
+// git checkout master                   перейти на ветку мастер
+// git pull                              чтобы стянуть изменения из ветки (делай только на ветке master)
+// git checkout -b branch_name           создаем ветку и переходим на неё
+// git add .                             класть все изменения в коробочку
+// git commit -m "Example comment"       закрываем коробочку и подписываем её
+// git push -u origin branch_name        отправить ветку в репозиторий
