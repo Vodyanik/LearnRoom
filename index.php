ac81f33b23aa470dae3daa537021e09a7f5af587
Vodyanik <vodyanik.artur@gmail.com>
2025-05-04 18:52:40 +0300

    [master]

--- index.php
+++ index.php
@@ -11,30 +11,30 @@ require_once 'helpers.php';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7"
-          crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 </head>
 <body>
     <div class="container-fluid">
         <div class="row">
+            <!-- SIDEBAR -->
             <?php echo sidebar(); ?>
+
             <div class='col-10 text-center'>
-            <?php
+                <?php
 
-            if (isset($_GET['task'])) {
-                $taskFile = __DIR__ . '/' . $_GET['task'] . '.php';
+                if (isset($_GET['task'])) {
+                    $taskFile = __DIR__ . '/' . $_GET['task'] . '.php';
 
-                if (file_exists($taskFile)) {
-                    require_once $taskFile;
+                    if (file_exists($taskFile)) {
+                        require_once $taskFile;
+                    } else {
+                        echo "<p>Файл не найден.</p>";
+                    }
                 } else {
-                    echo "<p>Файл не найден.</p>";
+                    echo "<p>Выберите задание слева.</p>";
                 }
-            } else {
-                echo "<p>Выберите задание слева.</p>";
-            }
 
-            ?>
+                ?>
             </div>
         </div>
     </div>
