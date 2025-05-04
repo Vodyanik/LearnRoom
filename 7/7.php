04c855d0e850846e1a12749ca2c8ea3a7e0519c1
Vodyanik <vodyanik.artur@gmail.com>
2025-05-04 18:44:30 +0300

    [vodyanik-DEV-27] easy template

--- 7/7.php
+++ 7/7.php
@@ -1,25 +1,19 @@
-<div style="margin: 30px; text-align: center;">
-	<a href="/">Home...</a>
-	<h3>Условие задачи</h3>
-	<p>	Определить количество натуральных чисел из интервала от 100 до 500, сумма цифр которых равна 15.
-	</p>
-	<h3>Результат: </h3>
-</div>
-
 <?php
+
+echo outputTaskConditionTemplate('Определить количество натуральных чисел из интервала от 100 до 500, сумма цифр которых равна 15.');
 
 $count = 0;
 
 for ($i = 100; $i <= 500; $i++) {
-	$one = (int) $i / 100;
-	$two = (int) ($i / 10) % 10;
-	$three = (int) $i % 10;
-	$sum = (int) ($one + $two + $three);
+    $one = (int) $i / 100;
+    $two = (int) ($i / 10) % 10;
+    $three = (int) $i % 10;
+    $sum = (int) ($one + $two + $three);
 
-	if ($sum === 15) {
-		echo $i . "  ";
-		$count++;
-	}
+    if ($sum === 15) {
+        echo $i . "  ";
+        $count++;
+    }
 }
 
 echo "<br>" . "Выведено $count раз";
