6d2b34b18f5612e405c1eed90552aa7df44b9a9d
alex1990win <kavalersanyok@gmail.com>
2025-05-21 23:26:31 +0300

    alex-32

--- 7/table.php
+++ 7/table.php
@@ -3,6 +3,7 @@
 		<title> Таблица </title> 
 	</head>
 	<body>
+		<a href="http://google.com" target="_blank"> google </a>
 		<h1> Таблица исследований </h1>
 		<ul>
 			<li> Cоздать таблицу с 3 колонками </li>
@@ -11,7 +12,7 @@
 			<li> Добавить в таблицу картинку </li>
 		</ul>
 		<hr>
-		<table>
+		<table border="1" style="color:purple;">
 			<caption> Табличка фруктовая </caption>
 			<thead>
 				<tr>
@@ -24,12 +25,16 @@
 				<tr>
 					<td> Яблоко </td>
 					<td> 2 </td>
-					<td> 3 </td>
+					<td>
+						<img src="https://kievflower.com.ua/published/publicdata/PENTAGON/attachments/SC/products_pictures/%D0%AF%D0%B1%D0%BB%D0%BE%D0%BA%D0%B8.jpg" width="150">
+					</td>
 				</tr>
 				<tr>
 					<td> Апельсин </td>
-					<td> 1 </td>
-					<td> 2 </td>
+					<td> 10 </td>
+					<td>
+						<img src="https://freshmart.com.ua/storage/web/cache/product/844/apelysin-chervoniy.jpg?w=912&h=690&fit=resize&q=80&fm=pjpg&t=1644926103&s=f1b8bb334fd55301dd09935fcfad6b04" width="150">
+					</td>
 				</tr>
 			</tbody>
 		</table>
