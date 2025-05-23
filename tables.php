972c679cfd25b0870188ea93135f79dd657df6bf
Vodyanik <vodyanik.artur@gmail.com>
2025-05-23 20:52:29 +0300

    [alex-32] example

--- tables.php
+++ tables.php
@@ -1,42 +1,47 @@
 <html>
-	<head>
-		<title> Таблица </title> 
-	</head>
-	<body>
-		<a href="http://google.com" target="_blank"> google </a>
-		<h1> Таблица исследований </h1>
-		<ul>
-			<li> Cоздать таблицу с 3 колонками </li>
-			<li> Выделить каждую строку таблицы разным цветом </li>
-			<li> Создать отдельно файл "style.css" и привязать его к проекту </li>
-			<li> Добавить в таблицу картинку </li>
-		</ul>
-		<hr>
-		<table border="1" style="color:purple;">
-			<caption> Табличка фруктовая </caption>
-			<thead>
-				<tr>
-					<th> Наименование фрукта </th>
-					<th> Количество </th>
-					<th> Фотография </th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td> Яблоко </td>
-					<td> 10 </td>
-					<td>
-						<img src="https://scontent.fiev15-1.fna.fbcdn.net/v/t39.30808-6/486819106_1856785491823848_1649290478170737161_n.jpg?stp=dst-jpg_s600x600_tt6&_nc_cat=109&ccb=1-7&_nc_sid=aa7b47&_nc_ohc=AP_KJ069qLAQ7kNvwHzcI0f&_nc_oc=Adldlrvv6D2MLwKqDYrlLacbz2oXAShRrDy4oWz6oCP4nHbjTHCOvmY5C1_vLsa3gDU&_nc_zt=23&_nc_ht=scontent.fiev15-1.fna&_nc_gid=xaKXz2SrDcyWCbc7DfgtxA&oh=00_AfIDs-lbZ5K8kXOBLzdshvME3Fk13r2b8Y9MhPST2PERng&oe=683428ED" width="150">
-					</td>
-				</tr>
-				<tr>
-					<td> Апельсин </td>
-					<td> 4 </td>
-					<td>
-						<img src="https://murchalkin.ru/post_images/shares/3eb288d4d84bc5c83e4f0b09470a2483-min.jpg" width="150">
-					</td>
-				</tr>
-			</tbody>
-		</table>
-	</body>
+<head>
+    <title> Таблица </title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barriecito&family=Mystery+Quest&display=swap" rel="stylesheet">
+</head>
+<body>
+<a href="http://google.com" target="_blank"> google </a>
+<h1> Таблица исследований </h1>
+<ul>
+    <li> Whereas recognition of the inherent dignity </li>
+    <li class="print-color-red"> Create table with 3 columns</li>
+    <li> Выделить каждую строку таблицы разным цветом</li>
+    <li class="print-color-red"> Создать отдельно файл "style.css" и привязать его к проекту</li>
+    <li> Добавить в таблицу картинку</li>
+</ul>
+<hr>
+<table>
+    <caption> Табличка фруктовая</caption>
+    <thead>
+    <tr>
+        <th> Наименование фрукта</th>
+        <th> Количество</th>
+        <th class="print-color-red"> <p>Фотография</p></th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td class="print-color-red"> Яблоко</td>
+        <td> 10</td>
+        <td>
+            <img src="images/apples.webp" width="150">
+        </td>
+    </tr>
+    <tr>
+        <td> Апельсин</td>
+        <td> 4</td>
+        <td>
+            <img src="images/oranges.jpg" width="150">
+        </td>
+    </tr>
+    </tbody>
+</table>
+</body>
 </html>
